c5765f5cf099f81961bdae16080bd6947fdef888
Smilator <116179692+Smilator@users.noreply.github.com>
2025-05-03 11:42:35 +0200

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,14 @@
+// db.js
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
-const dbPath = path.resolve(__dirname, 'data', 'games.db');
+const fs = require('fs');
 
+const dbDir = path.resolve(__dirname, 'data');
+if (!fs.existsSync(dbDir)) {
+  fs.mkdirSync(dbDir);
+}
+
+const dbPath = path.resolve(dbDir, 'games.db');
 const db = new sqlite3.Database(dbPath);
 
 db.serialize(() => {
